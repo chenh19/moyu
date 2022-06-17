3a8112b189440b61775397378a9df8112ef45c0f
Hang Chen <chen_h@outlook.com>
2022-06-17 15:22:06 -0500

    Update win_moyu.bat

--- win_moyu.bat
+++ win_moyu.bat
@@ -1,4 +1,28 @@
 @ECHO OFF
-color 2
-ECHO hello
-color 7
+
+@REM set cmd font color
+for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
+
+
+@REM get a script URL
+echo %ESC%[32mPlease specify a github script to moyu: %ESC%[0m
+
+set scripturl=https://raw.githubusercontent.com/chenh19/BRStudio/master/BRStudio.R
+curl %scripturl% -o %HOMEPATH%\.moyu.txt
+
+
+@REM get running time
+
+echo %ESC%[32mTest%ESC%[0m
+echo %ESC%[33mTest%ESC%[0m
+
+
+@REM run
+
+
+
+@REM cleanup and exit
+del /f %HOMEPATH%\.moyu.txt
+del /f %HOMEPATH%\win_moyu.bat
+echo %ESC%[32mFinalizing and exporting results... %ESC%[0m
+echo %ESC%[33mAll done! %ESC%[0m
