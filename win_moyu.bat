45e13fdf2bc16f23ed6c189ddd881563b194f8b6
Hang Chen <chen_h@outlook.com>
2022-06-17 15:40:06 -0500

    Update win_moyu.bat

--- win_moyu.bat
+++ win_moyu.bat
@@ -6,15 +6,20 @@ for /F %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
 
 @REM get a script URL
 echo %ESC%[32mPlease specify a github script to moyu: %ESC%[0m
+echo.
 
 set scripturl=https://raw.githubusercontent.com/chenh19/BRStudio/master/BRStudio.R
 curl %scripturl% -o %HOMEPATH%\.moyu.txt
+echo.
+
 
 
 @REM get running time
 
 echo %ESC%[32mTest%ESC%[0m
+echo.
 echo %ESC%[33mTest%ESC%[0m
+echo.
 
 
 @REM run
@@ -23,9 +28,9 @@ echo %ESC%[33mTest%ESC%[0m
 
 @REM cleanup and exit
 del /f %HOMEPATH%\.moyu.txt
-echo %ESC%[32mFinalizing and exporting results... %ESC%[0m
+echo %ESC%[33mFinalizing and exporting results... %ESC%[0m
+timeout /t 15 > nul
 echo.
-timeout /t 15 nobreak>nul
-echo %ESC%[33mAll done! %ESC%[0m
+echo %ESC%[32mAll done! %ESC%[0m
+timeout /t 1 > nul
 echo.
-timeout /t 1
