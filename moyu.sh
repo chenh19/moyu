c5e83006d9f7144e05e05d7ea50a94b0be6e5d9d
Hang Chen <chen_h@outlook.com>
2022-06-17 02:10:34 -0500

    Update moyu.sh

--- moyu.sh
+++ moyu.sh
@@ -15,13 +15,11 @@ done
 echo ""
 wget -O ~/.moyu.txt $scripturl
 
-# get running time
+# get running time and run
 unset runningmin
 until [[ $runningmin == +([0-9]) ]] ; do
     read -r -p "$(echo -e $TEXT_YELLOW'Please specify a time duration to moyu (minutes): '$TEXT_RESET)"$' \n' runningmin
 done
-
-# run
 endtime=$(( $(date +%M) + $runningmin ))
 while [ $(date +%M) -lt $endtime ]; do
     echo -e " \n${TEXT_YELLOW}Proceeding to the next step... ${TEXT_RESET}"$' \n' && sleep 3
