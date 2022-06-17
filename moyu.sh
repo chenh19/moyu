d590dcbed010df46a19cdc0424675ce93ecf1c5f
Hang Chen <chen_h@outlook.com>
2022-06-17 00:38:38 -0500

    Update moyu.sh

--- moyu.sh
+++ moyu.sh
@@ -7,7 +7,7 @@ TEXT_GREEN='\e[1;32m'
 TEXT_RESET='\e[0m'
 
 unset scripturl
-echo ""
+echo -e " \n"
 read -p "$(echo -e $TEXT_YELLOW'Please specify an online script to moyu: '$TEXT_RESET)"$' \n' scripturl
 echo $scripturl
 wget -O ~/.moyu.txt $scripturl
